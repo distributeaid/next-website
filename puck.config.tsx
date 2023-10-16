8ddfb88e1365c471f3e0c173cd125d3be2038764
Taylor Fairbank <taylor@distributeaid.org>
2023-10-16 06:40:48 +0000

    feat(component): add Flex component

--- puck.config.tsx
+++ puck.config.tsx
@@ -1,5 +1,6 @@
 import type { Config } from "@measured/puck";
 
+import { FlexConfig } from "./src/components/Flex"
 import { HeadingConfig } from "./src/components/Heading"
 import { MarkdownConfig } from "./src/components/Markdown"
 import { ParagraphConfig } from "./src/components/Paragraph"
@@ -8,6 +9,7 @@ import { CalloutConfig } from "./src/components/Callout"
 
 export const config: Config = {
   components: {
+    Flex: FlexConfig,
     Heading: HeadingConfig,
     Markdown: MarkdownConfig,
     Paragraph: ParagraphConfig,
