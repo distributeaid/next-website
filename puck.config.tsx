f28016197c57ef484d737cfd4ae3840597fce981
Taylor Fairbank <taylor@distributeaid.org>
2023-10-16 00:08:23 +0000

    feat(component): add Callout component

--- puck.config.tsx
+++ puck.config.tsx
@@ -4,13 +4,15 @@ import { HeadingConfig } from "./src/components/Heading"
 import { MarkdownConfig } from "./src/components/Markdown"
 import { ParagraphConfig } from "./src/components/Paragraph"
 import { BlockquoteConfig } from "./src/components/Blockquote"
+import { CalloutConfig } from "./src/components/Callout"
 
 export const config: Config = {
   components: {
     Heading: HeadingConfig,
     Markdown: MarkdownConfig,
     Paragraph: ParagraphConfig,
-    Blockquote: BlockquoteConfig
+    Blockquote: BlockquoteConfig,
+    Callout: CalloutConfig
   },
 };
 
---
+++ src/components/Callout.tsx
@@ -0,0 +1,43 @@
+import { ComponentConfig, FieldLabel } from "@measured/puck"
+import { Callout } from "@radix-ui/themes"
+import * as Icons from '@radix-ui/react-icons'
+import { defaultIconPickerFieldProps, IconPickerField } from "../fields/IconPicker"
+
+type Props = {
+  icon: string;
+  text: string;
+  size: "1" | "2" | "3";
+  variant: "soft" | "surface" | "outline";
+}
+
+export const CalloutConfig: ComponentConfig<Props> = {
+  fields: {
+    icon: {
+      ...IconPickerField
+    },
+    text: { type: "text" },
+    size: { type: "select", label: "size", options: [{ label: "small", value: "1"}, { label: "regular", value: "2"}, { label: "large", value: "3"}] },
+    variant: { type: "select", label: "variant", options: [{ label: "soft", value: "soft"}, { label: "surface", value: "surface"}, { label: "outline", value: "outline"}] },
+  },
+  defaultProps: {
+    ...defaultIconPickerFieldProps,
+    text: "Heads up that...",
+    size: "2",
+    variant: "soft"
+  },
+  render: ({ icon, text, size, variant }) => {
+    return (
+      <Callout.Root size={size} variant={variant}>
+        {icon && (
+          <Callout.Icon>
+            {Icons[icon].render({})}
+          </Callout.Icon>
+        )}
+        <Callout.Text>
+          {text}
+        </Callout.Text>
+      </Callout.Root>
+    )
+  }
+}
+
